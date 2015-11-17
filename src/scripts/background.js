8fb9f8cc32dc3221f960cb067b6257b9adfa52b4
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2015-11-17 20:09:21 -0300

    se prepara background para nueva API de servidor

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,32 +1,57 @@
-import utils from '../lib/utils'
+import * as utils from '../lib/utils'
 
+const serverHost = 'http://localhost:3000'
 let popupURL = chrome.extension.getURL('popup.html')
 
 let toQueryString = function(object) {
     return '?' +
-    Object.keys(object).map(function(key) {
-        return `${encodeURIComponent(key)}=${encodeURIComponent(object[key])}`
-    }).join('&');
+    Object.keys(object).map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(object[key])}`
+    ).join('&');
 }
 
-let processMessage = function(req, sender, callback){
+function postData(obj, kind, callback){
+  let xhr = new XMLHttpRequest()
+  xhr.open('POST', serverHost + '/scrap', true)
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+  xhr.send(JSON.stringify(obj))
+
+  let err = null
+  let response = {}
+  let success = false
+
+  xhr.addEventListener('error', (ev) => {
+    console.error(ev)
+    err = ev
+  })
+
+  xhr.addEventListener('load', (ev) => {
+    console.log(ev)
+    success = true
+    response['url'] = `${serverHost}/${kind}/${obj.code}`
+  })
+
+  xhr.addEventListener('loadend', (ev) => {
+    console.log(success)
+    callback(err, response)
+  })
+}
+
+function processMessage(req, sender, callback){
   if(req.type === 'info'){
-    let obj = req.data
-    obj.kind = req.kind
-    delete obj.teacherNode
-    delete obj.courseNode
-    delete obj.node
-    chrome.windows.create({
-      'url': `${popupURL}${toQueryString(obj)}`,
-      'type': 'popup',
-      'width': 550,
-      'height': 400,
-      'top': 200,
-      'left': 500
-    }, function(wnd) {
-      callback(wnd)
+    let obj = utils.stripObject(req.data)
+    postData(obj, req.kind, (err, response) => {
+      chrome.windows.create({
+        'url': response.url,
+        'type': 'popup',
+        'width': 550,
+        'height': 400,
+        'top': 200,
+        'left': 500
+      }, (popup) => { callback(popup) })
     })
   }
 }
+
 
 chrome.runtime.onMessage.addListener(processMessage)
