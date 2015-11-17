a8258c69511da8097e6e92c1e17d22be73e15b45
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2015-11-17 20:07:04 -0300

    se arreglan los nombres de los profesores

--- src/siga/utils.js
+++ src/siga/utils.js
@@ -21,3 +21,34 @@ export function onFrameReady(name, callback) {
     })
   }
 }
+
+export function fixTeacherName(name) {
+  let parts = name.toLowerCase().split(' ')
+  let delIdx = parts.indexOf('del')
+  if(delIdx > -1 && delIdx < parts.length - 1) {
+    let newParts = []
+    for(let i = 0; i < parts.length; i++) {
+      if(i != delIdx) newParts.push(parts[i])
+      else parts[i+1] = `${parts[i]} ${parts[i+1]}`
+    }
+    parts = newParts
+  }
+  switch(parts.length){
+    case 2: {
+      parts = parts.reverse()
+      break
+    }
+    case 3: {
+      parts = [parts[2], parts[0], parts[1]]
+      break
+    }
+    case 4: {
+      parts = [parts[2], parts[3], parts[0], parts[1]]
+      break
+    }
+  }
+
+  let lower = parts.filter((elem) => elem !== '.').join(' ')
+  return lower.replace(/(\S)(\S+\s?)/g, 
+    (_, initial, rest) => initial.toUpperCase() + rest.toLowerCase())
+}
